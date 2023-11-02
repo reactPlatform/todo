da9a4ba87536cc2d25e03ed75361d08b28f7fa40
kavi <sumathi291098@gmail.com>
2023-11-02 18:35:45 +0530

    Reflecting on the accomplishments

--- pages/completedTask.js
+++ pages/completedTask.js
@@ -1,13 +1,32 @@
 import React from 'react'
-
-const completedTask = (completedItems) => {
-
-  console.log(completedItems)
+import styles from '../styles/Home.module.css'
+const completedTask = (props) => {
+  const completedTodos = props.todoList;
   return (
-    <div style={{color:'white'}}>completedTask
+    <div className={styles.todoList}>
+      {
+            completedTodos.map(item =>
+            (
+              <div className={styles.todo} key={item.id}>
+                <span>{item.todoItem}</span>
+              </div>
+            )
+            )
+
+          }
     
     </div>
   )
 }
 
 export default completedTask
+export async function getServerSideProps(){
+  const response = await fetch('http://localhost:3000/api/todoHandler');
+  const dataResponse = await response.json();
+  const completedTodos = dataResponse.todoList.filter(item => item.isCompleted)
+  return{
+    props:{
+      todoList: completedTodos,
+    }
+  }
+}
